bc2516894ddc22b0a1bcf522e7888a66d109e31a
smortezah <s_morteza_h@yahoo.com>
2022-02-12 16:31:15 +0000

    Adding to __all__

--- napr/__init__.py
+++ napr/__init__.py
@@ -1,3 +1,8 @@
+from napr import (
+    applications,
+    data,
+    plotting
+)
 
 __version__ = '0.1.0'
 __name__ = 'napr'
@@ -5,4 +10,4 @@ __package__ = __name__
 __author__ = 'Morteza Hosseini'
 __license__ = 'GPLv3'
 
-__all__ = []
+__all__ = data.__all__ + applications.__all__ + plotting.__all__
