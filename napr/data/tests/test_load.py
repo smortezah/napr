60c136d9d729a50c69801c2a562ff73e713add62
smortezah <s_morteza_h@yahoo.com>
2022-03-13 16:23:30 +0000

    Test the data API

---
+++ napr/data/tests/test_load.py
@@ -0,0 +1,12 @@
+"""Test the data loading functions."""
+
+import pytest
+
+from napr.data._load import load_terpene
+
+
+def test_load_terpene():
+    """Test loading the terpene dataset."""
+    # Supported version is 21.3
+    with pytest.raises(ValueError):
+        load_terpene(download=False, version='20')
