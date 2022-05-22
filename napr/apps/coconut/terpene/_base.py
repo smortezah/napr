7fca7517ba7297e7261f741637c1e7025802692d
Morteza Hosseini <s_morteza_h@yahoo.com>
2022-05-22 19:07:07 +0100

    Change in terpene plot

--- napr/apps/coconut/terpene/_base.py
+++ napr/apps/coconut/terpene/_base.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-from napr.plotting import set_plt_style
+from napr import plotting
 from . import explore
 
 
@@ -13,24 +13,6 @@ class Terpene:
         self.data = data
 
         # Default plot style is 'ggplot_classic'
-        set_plt_style()
+        plotting.set_plt_style()
 
-    def plot_dist_subclass_mw_logp_nplscore(
-        data: pd.DataFrame, figsize: tuple[float, float] = (9, 8)
-    ) -> tuple[figure.Figure, list[axes.Axes]]:
-        explore._plot_dist_subclass_mw_logp_nplscore(data, figsize)
-
-    def plot_violin_mw_logp_nplscore(
-        data: pd.DataFrame, figsize: tuple[float, float] = (8.5, 3.5)
-    ) -> tuple[figure.Figure, list[axes.Axes]]:
-        explore._plot_violin_mw_logp_nplscore(data, figsize)
-
-    def plot_lipinsky(
-        data: pd.DataFrame, figsize: tuple[float, float] = (6, 8)
-    ) -> tuple[figure.Figure, list[axes.Axes]]:
-        explore._plot_lipinsky(data, figsize)
-
-    def plot_hbond(
-        data: pd.DataFrame, figsize: tuple[float, float] = (7, 10)
-    ) -> tuple[figure.Figure, list[axes.Axes]]:
-        explore._plot_hbond(data, figsize)
+        self.plot = explore.Plot(self.data)
