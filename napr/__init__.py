9c2cebcbc0dcdf4b6859bd6ce858a71ce1afdf27
Morteza Hosseini <s_morteza_h@yahoo.com>
2022-07-01 18:31:23 +0100

    Added 'evaluation' & 'hyperopt' submodules

--- napr/__init__.py
+++ napr/__init__.py
@@ -1,9 +1,9 @@
 """Napr package."""
 
-from napr import apps, data, plotting, utils
+from napr import apps, data, plotting, utils, evaluation, hyperopt
 
 __version__ = "0.1.5"
 __author__ = "Morteza Hosseini"
 __license__ = "MIT"
 
-__all__ = ["apps", "data", "plotting", "utils"]
+__all__ = ["apps", "data", "plotting", "utils", "evaluation", "hyperopt"]
