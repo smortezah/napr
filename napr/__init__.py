19a06e606b3128ea9c04eb071135c8fbbd5bde55
Morteza Hosseini <s_morteza_h@yahoo.com>
2026-06-02 10:38:38 +0100

    refactor: move from poetry to uv

--- napr/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Napr package."""
-
-from napr import apps, data, plotting, utils, evaluation, hyperopt
-
-__version__ = "0.1.5"
-__author__ = "Morteza Hosseini"
-__license__ = "MIT"
-
-__all__ = ["apps", "data", "plotting", "utils", "evaluation", "hyperopt"]
---
+++ src/napr/__init__.py
@@ -0,0 +1,5 @@
+"""Napr package."""
+
+from napr import apps, data, evaluation, hyperopt, plotting, utils
+
+__all__ = ["apps", "data", "plotting", "utils", "evaluation", "hyperopt"]
